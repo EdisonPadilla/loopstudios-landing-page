ba7d57c515cf931b073e8d5fdbbd62e28de78de1
Evanny <hbpgvanny@gmail.com>
2022-03-09 22:14:34 -0500

    desktop, mobile completo

--- js-functionality/mobile-menu.js
+++ js-functionality/mobile-menu.js
@@ -3,14 +3,12 @@
 const d = document;
 const menu_button = d.querySelector(".ham-btn");
 const bars = d.querySelectorAll(".ham-btn span");
-console.log(bars);
+const header = d.querySelector("header");
 const menu = d.querySelector(".nav-menu");
-console.log(menu);
-const nav = d.querySelector("header nav");
-console.log(nav);
+
 menu_button.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
-  nav.classList.toggle("change-background");
+  header.classList.toggle("change-background");
   bars.forEach((child) => {
     console.log(child);
     child.classList.toggle("animation");
